c5a3c9c476f6e25c3d0f7a345954c35abeee344c
davide-col <dav.colaci@gmail.com>
2022-03-21 04:54:04 +0100

    Docker menu fix

--- src/run_docker.sh
+++ src/run_docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 ##################################################
 ############ PREPARATION ENVIRONNEMENT ###########
@@ -7,68 +8,74 @@
 ############################################
 ############ PREPARATION DONNEES ###########
 
+cd ..
 echo "Unziping data..."
 unzip data.zip > /dev/null
+cd src
 
 ############################################
 ################ EXPÉRIENCES ###############
 
-function IdeaPOC {
-echo
-echo "###################################################"
-echo "Running all experiments without neural network..."
-echo "###################################################"
-echo
-python3 IdeaPOC.py
-echo
-echo "###################################################"
-}
-
-function monolingual_cv {
-
-echo
-echo "###################################################"
-echo "Running monolingual neural network experiments..."
-echo "###################################################"
-echo
-
-for lang in DE IT CZ
-do
-    echo "###### LANG: ${lang}"
-    python3 monolingual_cv.py ../data/${lang}
+IdeaPOC () {
     echo
-done
-
-echo "###################################################"
+    echo "###################################################"
+    echo "Running all experiments without neural network..."
+    echo "###################################################"
+    echo
+    python3 IdeaPOC.py
+    echo
+    echo "###################################################"
 }
 
-function multi_lingual {
-echo
-echo "###################################################"
-echo "Running multilingual neural network experiments..."
-echo "###################################################"
-echo
-python3 multi_lingual.py ../data
-echo
-echo "###################################################"
+monolingual_cv () {
+    echo
+    echo "###################################################"
+    echo "Running monolingual neural network experiments..."
+    echo "###################################################"
+    echo
+
+    for lang in DE IT CZ
+    do
+        echo "###### LANG: ${lang}"
+        python3 monolingual_cv.py ../data/${lang}
+        echo
+    done
+
+    echo "###################################################"
 }
 
-function multi_lingual_no_langfeat {
-echo
-echo "###################################################"
-echo "Running crosslingual neural network experiments without lang..."
-echo "###################################################"
-echo
-python3 multi_lingual_no_langfeat.py ../data
-echo
-echo "###################################################"
+multi_lingual () {
+    echo
+    echo "###################################################"
+    echo "Running multilingual neural network experiments..."
+    echo "###################################################"
+    echo
+    python3 multi_lingual.py ../data
+    echo
+    echo "###################################################"
+}
+
+multi_lingual_no_langfeat () {
+    echo
+    echo "###################################################"
+    echo "Running crosslingual neural network experiments without lang..."
+    echo "###################################################"
+    echo
+    python3 multi_lingual_no_langfeat.py ../data
+    echo
+    echo "###################################################"
 }
 
 echo "###################################################"
 echo "#                      MENU                       #"
 echo "###################################################"
 PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Option 4" "Option 5" "Quit")
+options=("Option 1 : All experiments" \
+"Option 2 : Experiments without neural network" \
+"Option 3 : Monolingual neural network experiments" \
+"Option 4 : Multilingual neural network experiments" \
+"Option 5 : Crosslingual neural network experiments without lang" \
+"Quit")
 select opt in "${options[@]}"
 do
     case $opt in
